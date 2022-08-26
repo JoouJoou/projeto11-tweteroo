6d5f741e3ab3e9066c7bbe5452f7fc6aa2c53b74
JouJou <joujou@pop-os.localdomain>
2022-08-26 04:04:47 -0300

    Reading tweets

--- index.js
+++ index.js
@@ -42,41 +42,27 @@ const tweets = [
     avatar: 'https://i.pinimg.com/474x/7e/03/00/7e0300453428cdd3dd9883ebc35862f9.jpg',
     tweet: 'Eu vou ser o rei dos piratas!!'
     },
-    {
-    username: 'Luffy',
-    avatar: 'https://i.pinimg.com/474x/7e/03/00/7e0300453428cdd3dd9883ebc35862f9.jpg',
-    tweet: 'Eu vou ser o rei dos piratas!!'
-    },
-    {
-    username: 'Luffy',
-    avatar: 'https://i.pinimg.com/474x/7e/03/00/7e0300453428cdd3dd9883ebc35862f9.jpg',
-    tweet: 'Eu vou ser o rei dos piratas!!'
-    },
-    {
-    username: 'Sanji',
-    avatar: 'https://sm.ign.com/t/ign_br/screenshot/default/sanji-one-piece_fv5j.h720.jpg',
-    tweet: 'Vou encontrar o All Blue!'
-    },
-    {
-    username: 'Zoro',
-    avatar: 'https://johto.legiaodosherois.com.br/wp-content/uploads/2021/11/legiao_XHYJRSWAV1xl.jpg',
-    tweet: 'Vou ser o maior espadachim!'
-    },
 ]
+
 
 app.post("/sign-up", (req, res) => {
     users.push(req.body)
-    console.log(users)
     res.send("OK")
 })
 
 app.get("/tweets", (req, res) => {
     const lastTweets = []
-    if (tweets.length() > 9) {
-        for (let i = 0; i <= 10; i++) {
+    if (tweets.length > 9) {
+        for (let i = 0; i < 10; i++) {
             lastTweets.push(tweets.pop())
         }
     }
+    else {
+        for (let i of tweets) {
+            lastTweets.push(i)
+        }
+    }
+    res.send(lastTweets)
 })
 
 app.listen(5000, () => {console.log("Listen on 5000")})
